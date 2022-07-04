2fb021994b17db663c46f3f09ded316303dadd72
M1089121 <vasanth.kumar@mindtree.com>
2022-07-04 05:56:19 -0700

    pushing selenium

--- selenium1.java
+++ selenium1.java
@@ -1,7 +1,12 @@
 package com.mindtree.seleniumclass;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class selenium1 {
 
@@ -9,7 +14,12 @@ public class selenium1 {
  
 		System.setProperty("webdriver.chrome.driver","‪./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com/");
+		driver.get("https://www.moneycontrol.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		Actions action = new Actions(driver);
+		
+		action.moveToElement(driver.findElement(By.linkText("Personal Finance"))).build().perform();
+		driver.findElement(By.linkText("Income Tax Calculator")).click(;)
 
 	}
 
